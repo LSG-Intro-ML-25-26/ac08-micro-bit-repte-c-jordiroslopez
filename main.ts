e0016eb8705892eaa8e25bd3fdffea09f67bdd03
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-12-01 22:13:27 +0100

    Sistema de comunicacion entre microbits

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let dice = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber >= dice) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    dice = randint(1, 6)
+    radio.sendNumber(dice)
+    basic.showNumber(dice)
+})
 basic.forever(function () {
-	
+    radio.setGroup(1)
 })
