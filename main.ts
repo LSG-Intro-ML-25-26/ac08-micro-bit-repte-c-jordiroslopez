6db3b6e453304f5a90e12dbdc10c56a691cfffd8
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-12-04 17:45:36 +0100

    feat: sistema para enviar texto

--- main.ts
+++ main.ts
@@ -5,6 +5,12 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onButtonPressed(Button.A, function () {
+    radio.sendString("helloworld!")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
     dice = randint(1, 6)
     radio.sendNumber(dice)
     basic.showNumber(dice)
